26bc8475689725fbe97ff1ae56551e756aade2fe
Elkayam Melesse <elka@duck.com>
2023-03-10 12:21:26 +0200

    init commit

---
+++ neural-network/test.py
@@ -0,0 +1,23 @@
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+
+model = keras.models.load_model("./saved_model")
+
+alphabet_dict = {0: 'א', 1: 'ב', 2: 'ג', 3: 'ד', 4: 'ה', 5: 'ו', 6: 'ז', 7: 'ח', 8: 'ט',
+                 9: 'י', 10: 'כ', 11: 'ן', 12: 'ל', 13: 'מ', 14: 'ם', 15: 'נ', 16: 'ן', 17: 'ס', 18: 'ע', 19: 'פ', 20: 'ף', 21: 'צ', 22: 'ץ', 23: 'ק', 24: 'ר', 25: 'ש', 26: 'ת'}
+
+img_height = 28
+img_width = 28
+
+print("Predictions:")
+image = tf.keras.preprocessing.image.load_img(
+    "Hebrew_Dataset/0/0_82.png", color_mode="grayscale", target_size=(img_height, img_width))
+
+input_arr = tf.keras.preprocessing.image.img_to_array(image)
+input_arr = np.array([input_arr])  # Convert single image to a batch.
+predictions = model.predict(input_arr)
+
+print(predictions)
+print("the letter index is: " + str(np.argmax(predictions)))
+print("the predited result: " + alphabet_dict(np.argmax(predictions)))
---
+++ neural-network/train.py
@@ -0,0 +1,66 @@
+# Imports needed
+import tensorflow as tf
+from tensorflow import keras
+from keras import layers
+import os
+import numpy as np
+
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+
+img_height = 28
+img_width = 28
+batch_size = 16
+
+model = keras.Sequential(
+    [
+        layers.Input((28, 28, 1)),
+        layers.Conv2D(16, 3, padding="same"),
+        layers.Conv2D(32, 3, padding="same"),
+        layers.MaxPooling2D(),
+        layers.Flatten(),
+        layers.Dense(27),
+    ]
+)
+
+ds_train = tf.keras.preprocessing.image_dataset_from_directory(
+    "../dataset",
+    labels="inferred",
+    label_mode='int',  # categorical, binary
+    # class_names=['0', '1', '2', '3', ...]
+    color_mode="grayscale",
+    batch_size=batch_size,
+    image_size=(img_height, img_width),  # reshape if not in this size
+    shuffle=True,
+    seed=123,
+    validation_split=0.1,
+    subset="training",
+)
+
+ds_validation = tf.keras.preprocessing.image_dataset_from_directory(
+    "Hebrew_Dataset",
+    labels="inferred",
+    label_mode="int",  # categorical, binary
+    # class_names=['0', '1', '2', '3', ...]
+    color_mode="grayscale",
+    batch_size=batch_size,
+    image_size=(img_height, img_width),  # reshape if not in this size
+    shuffle=True,
+    seed=123,
+    validation_split=0.1,
+    subset="validation",
+)
+
+model.compile(
+    optimizer=keras.optimizers.Adam(),
+    loss=[keras.losses.SparseCategoricalCrossentropy(from_logits=True),],
+    metrics=["accuracy"],
+)
+
+print("\nFit model on training data:")
+model.fit(ds_train, epochs=30, verbose=2)
+model.summary()
+
+print("\nEvaluate on test data:")
+model.evaluate(ds_validation, verbose=2)
+
+model.save("saved_model/")
