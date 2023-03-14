1f77c49b7fc9441b6aead56841a2270f89ceaa83
Elkayam Melesse <elka@duck.com>
2023-03-14 14:25:53 +0200

    cropping pdfs script

--- neural-network/processing pdfs/pdf_to_letters.py
+++ neural-network/processing pdfs/pdf_to_letters.py
@@ -1,21 +1,36 @@
-import fitz  # PyMuPDF
+import PyPDF2
+from PIL import Image
 
-# Open the PDF file
-doc = fitz.open('neural-network\processing pdfs\elka-template.pdf')
 
-# Iterate over each page in the PDF
-for page in doc:
-    # Extract the text from the page
-    text = page.extract_text()
-    print(text)
-    # Split the text into individual letters
-    letters = text.split()
-    # Iterate over each letter and extract it as an image
-    for letter in letters:
-        # Get the bounding box of the letter
-        bbox = page.search_for(letter)[0]
-        # Extract the letter as an image
-        letter_image = page.get_pixmap(alpha=False, clip=bbox)
-        # Save the letter image to a file
-        letter_image.save(
-            "neural-network\processing pdfs/output-images"/f'{letter}.png')
+pdf_file = open('neural-network/processing pdfs/elka-template.pdf', 'rb')
+pdf_reader = PyPDF2.PdfReader(pdf_file)
+
+# Iterate through each page of the PDF
+for page_num in range(len(pdf_reader.pages)):
+    page = pdf_reader.pages[page_num]
+
+    page_width = int(page.mediabox.width)
+    page_height = int(page.mediabox.height)
+
+    # Calculate the width and height of each square
+    square_width = page_width // 9
+    square_height = page_height // 13
+
+    for row in range(13):
+        for col in range(9):
+
+            # Calculate the coordinates of the top-left corner of the square
+            x0 = col * square_width
+            y0 = page_height - (row + 1) * square_height
+            x1 = x0 + square_width
+            y1 = y0 + square_height
+
+            square = page.cropbox((x0, y0, x1, y1))
+
+            # Save the square as an image
+            image = square.convert('RGB')
+            image.save(
+                "neural-network/processing pdfs/output-images"f'page{page_num+1}_row{row+1}_col{col+1}.jpg')
+
+
+pdf_file.close()
