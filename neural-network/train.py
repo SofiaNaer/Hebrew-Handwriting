db62db45f7d1e1a66a4e39781e442d6c7f9f3211
Elkayam Melesse <elka@duck.com>
2023-03-17 14:59:54 +0200

    updated data. acc: 0.82 -- loss: 1.28

--- neural-network/train.py
+++ neural-network/train.py
@@ -56,7 +56,7 @@ model.compile(
 )
 
 print("\nFit model on training data:")
-model.fit(ds_train, epochs=20, verbose=2, validation_data=ds_validation)
+model.fit(ds_train, epochs=30, verbose=2, validation_data=ds_validation)
 model.summary()
 
 print("\nEvaluate on validation data:")
